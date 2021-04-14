e33895aa2abbf39810b6b21a805773aeffaeb96b
ULISES CECA <ulises@ulisesceca.com>
2021-04-14 10:59:56 +0200

    done tests for LoginPanelComponent

--- src/app/components/login/login-panel/login-panel.component.spec.ts
+++ src/app/components/login/login-panel/login-panel.component.spec.ts
@@ -24,4 +24,38 @@ describe('LoginPanelComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('#submitForm() emits an onFormSuccess event', () => {
+    component.loginForm.get('email').setValue('test@test.com');
+    component.loginForm.get('password').setValue('12345');
+    component.loginForm.get('remember').setValue(true);
+
+    spyOn(component.onFormSuccess, 'emit');
+    fixture.detectChanges();
+
+    component.submitForm();
+    
+    expect(component.onFormSuccess.emit).toHaveBeenCalled();
+  });
+
+  it('#submitForm() emits an onFormFail event', () => {
+    component.loginForm.get('email').setValue('test@test.com');
+    component.loginForm.get('password').setValue('1234');
+    component.loginForm.get('remember').setValue(true);
+
+    spyOn(component.onFormFail, 'emit');
+    fixture.detectChanges();
+
+    component.submitForm();
+    
+    expect(component.onFormFail.emit).toHaveBeenCalled();
+  });
+
+  it('#getFormErrors() returns that the field password is not correct', () => {
+    component.loginForm.get('email').setValue('test@test.com');
+    component.loginForm.get('password').setValue('');
+    component.loginForm.get('remember').setValue(true);
+    
+    expect(component.getFormErrors(component.loginForm)).toEqual(['password']);
+  });
 });
