ade91aba2944f9e28fd186deb9e5bd93278090b0
ULISES CECA <ulises@ulisesceca.com>
2021-04-14 06:31:29 +0200

    icons

--- src/app/components/login/login.module.ts
+++ src/app/components/login/login.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPanelComponent } from './login-panel/login-panel.component';
 import { LoginComponent } from './login.component';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatInputModule } from '@angular/material/input';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 
@@ -15,8 +15,7 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    MatSlideToggleModule,
-    MatInputModule,
+    MatIconModule,
     ReactiveFormsModule
   ],
   exports: [
